a6601b8834a1933948eecc450932c5bbdb1eb56e
Ya'Kuana Davis <ydavis@bowdoin.edu>
2020-04-20 14:53:48 -0500

    WIP: Added DisplayArt to Database page.

--- project/src/components/ArtCard.js
+++ project/src/components/ArtCard.js
@@ -6,19 +6,37 @@ import '../styles/Art.css'
 
 function ArtCard({ art, photo }) {
     return (
-        <div className="art-card">
-            <img src={photo} alt={`${art.title} by ${art.artist}`} className="photo"/>
-            <div className="art-text-container">
-                <h2>{art.title}</h2>
-                <h3>{art.artist}</h3>
-                <p>{art.creation_date}</p>
-                <p>{art.type}; {art.medium_support}</p>
-                <p>{art.credit_line}</p>
-                {/* <p>{art.description}</p> */}
-                <p>{art.accession_number}</p>
+        <div className="flip-card">
+            <div className="flip-card-inner">
+                <div className="flip-card-front">
+                    <img src={photo} alt={`${art.title} by ${art.artist}`} className="flip-image"/>
+                </div>
+                <div className="flip-card-back">
+                    <h1>{art.title}</h1>
+                    <div id="flip-text">
+                        <p><strong>Artist</strong> - {art.artist}</p>
+                        <p><strong>Creation Date</strong> - {art.creation_date}</p>
+                        <p><strong>Medium and Support</strong> - {art.medium_support}</p>
+                        <p><strong>Credit Line</strong>- {art.credit_line}</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
 }
+
+// <div className="art-card">
+//             <img src={photo} alt={`${art.title} by ${art.artist}`} className="photo"/>
+//             <div className="art-text-container">
+//                 <h2>{art.title}</h2>
+//                 <h3>{art.artist}</h3>
+//                 <p>{art.creation_date}</p>
+//                 <p>{art.type}; {art.medium_support}</p>
+//                 <p>{art.credit_line}</p>
+//                 <p>{art.description}</p>
+//                 <p>{art.accession_number}</p>
+//             </div>
+//         </div>
+//         </> 
 
 export default ArtCard;
--- project/src/components/Database.js
+++ project/src/components/Database.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ArtList from './ArtList.js'
+import DisplayArt from './DisplayArt.js';
 
 function Database() {
     return (
         <div>
             <h1>Database</h1>
-            <ArtList/>
+            <div>
+                <h3>Token Art Piece</h3>
+                <DisplayArt />
+            </div>
+            <div>
+                <h3>Art Cards</h3>
+                <ArtList />
+            </div>
+            <h3>Art Locations</h3>
         </div>
     );
 }
