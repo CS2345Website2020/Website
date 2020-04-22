ba73039cd37402368e118f708fe457924697a97b
Emme <emmccabe@bowdoin.edu>
2020-04-21 21:00:10 -0400

    updated about.js

--- project/src/components/About.js
+++ project/src/components/About.js
@@ -10,10 +10,14 @@ import EmmeMcCabe from '../images/meet-the-team/emme-mccabe2.jpeg'
 import YakuanaDavis from '../images/meet-the-team/yakuana-davis.jpeg'
 import KateMorrison from '../images/meet-the-team/kate-morrison.jpeg'
 import FinnBergquist from '../images/meet-the-team/finn-bergquist.jpeg'
+import AhmedHameed from '../images/meet-the-team/ahmed-hameed.jpeg'
+import TheaKelsey from '../images/meet-the-team/thea-kelsey.jpeg'
+
 
 import git2 from '../images/meet-the-team/36675751.png'
 import git3 from '../images/meet-the-team/36677169.png'
 import git4 from '../images/meet-the-team/34076044.png'
+import git5 from '../images/meet-the-team/10201229.png'
 
 
 function About() {
@@ -58,16 +62,15 @@ function About() {
             <div class="column">
                 <img className="avatar" src={EmmeMcCabe} alt="Student Emme McCabe"/>
                 <h2><a href="https://www.linkedin.com/in/emme-mccabe" className="link">Emme McCabe '20</a></h2>
-                <p> Computer Science Major with an interest in the convergence of computing ethics and quantitative trading. </p>
+                <p> Computer Science Major exploring the convergence of computing ethics and quantitative trading. </p>
             </div>
-
                 
         </div>
 
         <h2 class="team-title"> Database Design </h2>
         <div class="flex-container">
             <div class="column">
-                <img className="avatar" src={git3} alt="Student Ahmed Hameed"/>
+                <img className="avatar" src={AhmedHameed} alt="Student Ahmed Hameed"/>
                 <h2><a href="https://www.linkedin.com/in/ahmed-hameed-521145166/" className="link">Ahmed Hameed '22</a></h2>
                 <p> Computer Science Major at Bowdoin College with an interest in Software Development. </p>
             </div>
@@ -75,6 +78,24 @@ function About() {
         </div>
 
         <h2 class="team-title"> UX/UI </h2>
+
+        <div class="flex-container">
+            <div class="column">
+                <img className="avatar" src={git2} alt="Student Nell Fusco '21"/>
+                <h2><a href="https://www.linkedin.com/in/nell-eleanore-fusco-b53412142/" className="link">Nell Fusco '21</a></h2>
+                <p> Computer Science and Government and Legal Studies double major with an interest in Software Engineering. </p>
+            </div>
+            <div class="column">
+                <img className="avatar" src={TheaKelsey} alt="Student Thea Kelsey '20"/>
+                <h2><a href="https://www.linkedin.com/in/cynthia-thea-kelsey-520b61128/" className="link">Thea Kelsey '20</a></h2>
+                <p> Sociology major and CS minor. </p>
+            </div>
+            <div class="column">
+                <img className="avatar" src={git5} alt="Student Bradley Ingersoll '21"/>
+                <h2><a href="#" className="link">Bradley Ingersoll '21</a></h2>
+                <p> Bio </p>
+            </div>
+        </div>
 
         <h2 class="team-title"> Mobile Application </h2>
 
@@ -87,7 +108,7 @@ function About() {
             <div class="column">
                 <img className="avatar" src={KateMorrison} alt="Student Kate Morrison"/>
                 <h2><a href="https://www.linkedin.com/in/kathleen-morrison-780372156/" className="link">Kate Morrison '20</a></h2>
-                <p>  </p>
+                <p> Mathematics major with an interest in data science and data visualization. </p>
             </div>
 
         </div>
@@ -95,6 +116,14 @@ function About() {
         <h2 class="team-title"> Data Collection </h2>
 
         <h2 class="team-title"> Alexa Voice Integration </h2>
+        <div class="flex-container">
+        <div class="column">
+                <img className="avatar" src={git2} alt="Student Henry Isaacson"/>
+                <h2><a href="https://www.linkedin.com/in/henry-isaacson-061b29196/" className="link">Henry Isaacson '22</a></h2>
+                <p> Computer Science Major with an interest in computational biology and genomics. </p>
+        </div>
+        </div>
+        
 
         <h2 class="team-title"> Tracking and Navigation </h2>
 
