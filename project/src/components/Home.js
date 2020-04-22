3a51b0a63d85f15f5c8c023312ee2371e62d6e34
Liam Juskevice <LiamJuskevice42@Gmail.com>
2020-04-22 00:22:19 -0400

    Updated Home Page
    I have officially made our website look like an even bigger knock-off of Bowdoin's site than before.

--- project/src/components/Home.js
+++ project/src/components/Home.js
@@ -3,10 +3,21 @@ import React from 'react';
 //style
 import "../styles/home.css"
 
+import GalleryPhoto from "../images/instruct-delight-2019-2.jpg"
+
 function Home() {
     return (
+        <div>
         <div className="banner">
-            <br></br>
+        <div className="shadow-boxing">
+        <h1 className="welcome">Welcome</h1>
+        <h2 className="home-text">To learn more about our project feel free to explore our menu above or 
+        check out our virtual tour of the Bowdoin Art Museum below (whenever it gets implemented)
+        </h2>
+        </div>
+        </div>
+        <br></br>
+        <img src={GalleryPhoto} className="virtual-tour" alt="Photo of room in art museum showcasing European and American works"></img>
         </div>
     );
 }
