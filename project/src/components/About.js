37d5aadbe3ac850bea88f729cf636f85acf48272
Emme <emmccabe@bowdoin.edu>
2020-04-22 15:11:20 -0400

    updated about

--- project/src/components/About.js
+++ project/src/components/About.js
@@ -12,8 +12,12 @@ import KateMorrison from '../images/meet-the-team/kate-morrison.jpeg'
 import FinnBergquist from '../images/meet-the-team/finn-bergquist.jpeg'
 import AhmedHameed from '../images/meet-the-team/ahmed-hameed.jpeg'
 import TheaKelsey from '../images/meet-the-team/thea-kelsey.jpeg'
+import TravisRase from '../images/meet-the-team/travis-rase.jpeg'
+import AnaisSarrazin from '../images/meet-the-team/anais-sarrazin.jpeg'
+import NellFusco from '../images/meet-the-team/nell-fusco.jpeg'
+import HenryIsaacson from '../images/meet-the-team/henry-isaacson.jpeg'
 
-
+//git images
 import git2 from '../images/meet-the-team/36675751.png'
 import git3 from '../images/meet-the-team/36677169.png'
 import git4 from '../images/meet-the-team/34076044.png'
@@ -35,7 +39,7 @@ function About() {
                     <p> Spatial cognition and indoor navigation expert, UX/UI, Assistive Technologies, natural language and VR/AR applications </p>
                 </div>
                 <div class="column">
-                    <img className="avatar" src={git2} alt="Student Anaïs Sarrazin"/>
+                    <img className="avatar" src={AnaisSarrazin} alt="Student Anaïs Sarrazin"/>
                     <h2><a href="https://www.github.com/anaissarrazin" className="link">Anaïs Sarrazin '20</a></h2>
                     <p> Computer Science and Math Major </p>
                 </div>
@@ -81,7 +85,7 @@ function About() {
 
         <div class="flex-container">
             <div class="column">
-                <img className="avatar" src={git2} alt="Student Nell Fusco '21"/>
+                <img className="avatar" src={NellFusco} alt="Student Nell Fusco '21"/>
                 <h2><a href="https://www.linkedin.com/in/nell-eleanore-fusco-b53412142/" className="link">Nell Fusco '21</a></h2>
                 <p> Computer Science and Government and Legal Studies double major with an interest in Software Engineering. </p>
             </div>
@@ -110,6 +114,11 @@ function About() {
                 <h2><a href="https://www.linkedin.com/in/kathleen-morrison-780372156/" className="link">Kate Morrison '20</a></h2>
                 <p> Mathematics major with an interest in data science and data visualization. </p>
             </div>
+            <div class="column">
+                <img className="avatar" src={TravisRase} alt="Student Travis Rase"/>
+                <h2><a href="https://www.linkedin.com/in/travis-rase-763b9514b/" className="link">Travis Rase '20</a></h2>
+                <p> Computer Science major and Physics minor deeply passionate about software engineering and becoming a trophy husband.  </p>
+            </div>
 
         </div>
 
@@ -118,7 +127,7 @@ function About() {
         <h2 class="team-title"> Alexa Voice Integration </h2>
         <div class="flex-container">
         <div class="column">
-                <img className="avatar" src={git2} alt="Student Henry Isaacson"/>
+                <img className="avatar" src={HenryIsaacson} alt="Student Henry Isaacson"/>
                 <h2><a href="https://www.linkedin.com/in/henry-isaacson-061b29196/" className="link">Henry Isaacson '22</a></h2>
                 <p> Computer Science Major with an interest in computational biology and genomics. </p>
         </div>
