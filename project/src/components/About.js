77173f2b0cb7b17bb68337176282373093189b3f
Emme <emmccabe@bowdoin.edu>
2020-04-23 14:31:37 -0400

    updated about.js

--- project/src/components/About.js
+++ project/src/components/About.js
@@ -16,6 +16,16 @@ import TravisRase from '../images/meet-the-team/travis-rase.jpeg'
 import AnaisSarrazin from '../images/meet-the-team/anais-sarrazin.jpeg'
 import NellFusco from '../images/meet-the-team/nell-fusco.jpeg'
 import HenryIsaacson from '../images/meet-the-team/henry-isaacson.jpeg'
+import ConnorFitzgerald from '../images/meet-the-team/connor-fitzgerald.jpeg'
+import RussellOBrien from '../images/meet-the-team/russell-obrien.jpeg'
+import GeorgeBenz from '../images/meet-the-team/george-benz.jpeg'
+import HarrySherman from '../images/meet-the-team/harry-sherman.jpeg'
+import MaxTanous from '../images/meet-the-team/max-tanous.jpeg'
+import RyanDonlan from '../images/meet-the-team/ryan-donlan.jpeg'
+import TashaChrist from '../images/meet-the-team/tasha-christ.jpeg'
+import AdelaideEvans from '../images/meet-the-team/adelaide-evans.jpeg'
+import BradIngersoll from '../images/meet-the-team/brad-ingersoll.jpeg'
+
 
 //git images
 import git2 from '../images/meet-the-team/36675751.png'
@@ -51,7 +61,7 @@ function About() {
             <div class="column">
                 <img className="avatar" src={FinnBergquist} alt="Student Finn Bergquist "/>
                 <h2><a href="https://www.linkedin.com/in/finn-bergquist/" className="link">Finn Bergquist '22</a></h2>
-                <p> Bio </p>
+                <p> Computer Science and Physics double major with interest in musical technology. </p>
             </div>
             <div class="column">
                 <img className="avatar" src={YakuanaDavis} alt="Student Ya'Kuana Davis"/>
@@ -61,7 +71,7 @@ function About() {
             <div class="column">
                 <img className="avatar" src={git4} alt="Student Liam Juskevice"/>
                 <h2><a href="https://github.com/" className="link">Liam Juskevice '21</a></h2>
-                <p> Bio </p>
+                <p> Computer Science Major and aspiring tech bro. </p>
             </div> 
             <div class="column">
                 <img className="avatar" src={EmmeMcCabe} alt="Student Emme McCabe"/>
@@ -72,13 +82,27 @@ function About() {
         </div>
 
         <h2 class="team-title"> Database Design </h2>
-        <div class="flex-container">
+        <div class="flex-container">         
+            <div class="column">
+                <img className="avatar" src={RyanDonlan} alt="Student Ryan Donlan"/>
+                <h2><a href="https://www.linkedin.com/in/ryan-donlan-4912a1179/" className="link">Ryan Donlan '21</a></h2>
+                <p> Computer Science major with interests in software engineering, cyber security and big data. </p>
+            </div>
+            <div class="column">
+                <img className="avatar" src={ConnorFitzgerald} alt="Student Connor Fitzgerald"/>
+                <h2><a href="https://linkedin.com/in/cfitzgerald11" className="link">Connor Fitzgerald '20</a></h2>
+                <p> Environmental studies major with an interest in data engineering and sustainable computing. </p>
+            </div>
             <div class="column">
                 <img className="avatar" src={AhmedHameed} alt="Student Ahmed Hameed"/>
                 <h2><a href="https://www.linkedin.com/in/ahmed-hameed-521145166/" className="link">Ahmed Hameed '22</a></h2>
                 <p> Computer Science Major at Bowdoin College with an interest in Software Development. </p>
             </div>
-
+            <div class="column">
+                <img className="avatar" src={git2} alt="Student Isaac Kabuika"/>
+                <h2><a href="/#" className="link">Isaac Kabuika '20</a></h2>
+                <p> Bio </p>
+            </div>
         </div>
 
         <h2 class="team-title"> UX/UI </h2>
@@ -95,9 +119,9 @@ function About() {
                 <p> Sociology major and CS minor. </p>
             </div>
             <div class="column">
-                <img className="avatar" src={git5} alt="Student Bradley Ingersoll '21"/>
+                <img className="avatar" src={BradIngersoll} alt="Student Bradley Ingersoll '21"/>
                 <h2><a href="#" className="link">Bradley Ingersoll '21</a></h2>
-                <p> Bio </p>
+                <p> Computer Science major with an interest in big data and digital marketing. </p>
             </div>
         </div>
 
@@ -105,9 +129,9 @@ function About() {
 
         <div class="flex-container">
             <div class="column">
-                <img className="avatar" src={git3} alt="Student Adelaide Evans"/>
-                <h2><a href="#" className="link">Adelaide Evans '22</a></h2>
-                <p>  </p>
+                <img className="avatar" src={AdelaideEvans} alt="Student Adelaide Evans"/>
+                <h2><a href="https://www.linkedin.com/in/adelaide-evans-a21826198/" className="link">Adelaide Evans '22</a></h2>
+                <p> Computer Science Major with interests in software development. </p>
             </div>
             <div class="column">
                 <img className="avatar" src={KateMorrison} alt="Student Kate Morrison"/>
@@ -119,23 +143,63 @@ function About() {
                 <h2><a href="https://www.linkedin.com/in/travis-rase-763b9514b/" className="link">Travis Rase '20</a></h2>
                 <p> Computer Science major and Physics minor deeply passionate about software engineering.  </p>
             </div>
-
+            <div class="column">
+                <img className="avatar" src={MaxTanous} alt="Student Max Tanous"/>
+                <h2><a href="https://www.linkedin.com/in/max-tanous-210930140/" className="link">Max Tanous '20</a></h2>
+                <p> Computer Science major and full-stack developer. </p>
+            </div>
         </div>
 
         <h2 class="team-title"> Data Collection </h2>
+      
+        <div class="flex-container">
+        <div class="column">
+                <img className="avatar" src={TashaChrist} alt="Student Tasha Christ"/>
+                <h2><a href="https://www.linkedin.com/in/anastasia-christ-5b9960104/" className="link">Tasha Christ '20</a></h2>
+                <p> Neuroscience major and computer science minor with an interest in medicine </p>
+        </div>
+        <div class="column">
+                <img className="avatar" src={HarrySherman} alt="Student Harry Sherman"/>
+                <h2><a href="https://www.linkedin.com/in/harry-sherman/" className="link">Harry Sherman '21</a></h2>
+                <p> Computer Science and Education student interested in FinTech and EdTech </p>
+        </div>
+        </div>
 
         <h2 class="team-title"> Alexa Voice Integration </h2>
         <div class="flex-container">
+	
         <div class="column">
                 <img className="avatar" src={HenryIsaacson} alt="Student Henry Isaacson"/>
                 <h2><a href="https://www.linkedin.com/in/henry-isaacson-061b29196/" className="link">Henry Isaacson '22</a></h2>
                 <p> Computer Science Major with an interest in computational biology and genomics. </p>
         </div>
+        <div class="column">
+                <img className="avatar" src={RussellOBrien} alt="Student Russell O'Brien"/>
+                <h2><a href="https://www.linkedin.com/in/russell-o-brien-526639171/" className="link">Russell O'Brien '20</a></h2>
+                <p> Computer Science Major with interests in AI and Machine Learning </p>
+        </div>
         </div>
         
 
         <h2 class="team-title"> Tracking and Navigation </h2>
+        <div class="flex-container">
+        <div class="column">
+                <img className="avatar" src={GeorgeBenz} alt="Student George benz"/>
+                <h2><a href="https://www.linkedin.com/in/george-benz/" className="link">George Benz '20</a></h2>
+                <p> Full-time passion student at Bowdoin college looking for any kind of work in the Computer Science Sector and other relevant Sectors </p>
+        </div>
+        <div class="column">
+                <img className="avatar" src={git3} alt="Student Casey Edmonds-Estes"/>
+                <h2><a href="https://www.linkedin.com/in/casey-edmonds-estes-a87702183/" className="link">Casey Edmonds-Estes '22</a></h2>
+                <p> Computer Scientist, roboticist, fencer, bassoonist. </p>
+        </div> 
+        <div class="column">
+                <img className="avatar" src={git4} alt="Student Aly Hummel"/>
+                <h2><a href="https://www.linkedin.com/in/alexandra-hummel3/" className="link">Aly Hummel '21</a></h2>
+                <p> CS and Government major and Duolingo addict. </p>
+        </div>
 
+        </div>
 
         </div>
     
