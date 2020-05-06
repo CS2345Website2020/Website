9a32c95311a22a7688490d6cecdbffb629f2c286
Liam Juskevice <LiamJuskevice42@Gmail.com>
2020-05-06 11:12:29 -0400

    Home page update and rippling style fixes
    I didn't want padding at the bottom of one page so I had to readd it elsewhere

--- project/src/components/Database.js
+++ project/src/components/Database.js
@@ -4,7 +4,7 @@ import DisplayArt from './DisplayArt.js';
 
 function Database() {
     return (
-        <div>
+        <div className="bottom-padding">
             <h1>Database</h1>
             <div>
                 <h3 className="data-section-titles">Art of the Day</h3>
