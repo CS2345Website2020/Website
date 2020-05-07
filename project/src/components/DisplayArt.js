78b9d8d2203e9ddb140809372b3c557fef45a613
Ya'Kuana Davis <ydavis@bowdoin.edu>
2020-05-07 14:05:16 -0500

    Style updates

--- project/src/components/DisplayArt.js
+++ project/src/components/DisplayArt.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import "../styles/Art.css"
 
 // art image 
-import Img18551 from '../images/artwork/1813.13.jpg';
+import Img181313 from '../images/artwork/1813.13.jpg';
 
 function DisplayArt() {
 
@@ -33,20 +33,33 @@ function DisplayArt() {
     return (
         <>
         {art ? (
-            <div id="display-container">
-                {/* <img src={Img18551} alt={`${art.title} by ${art.artist}`} id="display-image"/> */}
-                <div>
-                    <div id="display-text">
-                        <h2 id="display-title">{art.title}</h2>
-                        <h3 id="display-artist">{art.artist}</h3>
-                    </div>
-                    <div id="display-info">
-                        <p><strong>Accession Number</strong> - {art.accession_number}</p>
-                        <p><strong>Type, Medium and Support</strong> - {art.type}, {art.medium_support}</p>
-                        <p><strong>Basic Description</strong> - {art.description_basic}</p>
-                        <p><strong>Spatial Description</strong> - {art.description_spatial}</p>
-                        <p><strong>Thematic Description</strong> - {art.description_thematic}</p>
-                    </div>
+            <div>
+                <div id="display-text">
+                    <h2 id="display-title">{art.title}</h2>
+                    <h3 id="display-artist">{art.artist}</h3>
+                </div>
+                <div id="display-container">
+                    <img src={Img181313} alt={`${art.title} by ${art.artist}`} id="display-image"/>
+                    {/* <div>
+                        <div id="display-text">
+                            <h2 id="display-title">{art.title}</h2>
+                            <h3 id="display-artist">{art.artist}</h3>
+                        </div>
+                        <div id="display-info">
+                            <p><strong>Accession Number</strong> - {art.accession_number}</p>
+                            <p><strong>Type, Medium and Support</strong> - {art.type}, {art.medium_support}</p>
+                            <p><strong>Basic Description</strong> - {art.description_basic}</p>
+                            <p><strong>Spatial Description</strong> - {art.description_spatial}</p>
+                            <p><strong>Thematic Description</strong> - {art.description_thematic}</p>
+                        </div>
+                    </div> */}
+                </div>
+                <div id="display-info">
+                    <p><strong>Accession Number</strong> - {art.accession_number}</p>
+                    <p><strong>Type, Medium and Support</strong> - {art.type}, {art.medium_support}</p>
+                    <p><strong>Basic Description</strong> - {art.description_basic}</p>
+                    <p><strong>Spatial Description</strong> - {art.description_spatial}</p>
+                    <p><strong>Thematic Description</strong> - {art.description_thematic}</p>
                 </div>
             </div>
         ) : <h1>Loading</h1>
