36b63b5015181635266a0a56d6259db66af25ea7
finnbergquist <fbergqui@bowdoin.edu>
2020-05-11 13:17:46 -0400

    History page updates

--- project/src/components/History.js
+++ project/src/components/History.js
@@ -11,6 +11,13 @@ import GalleryPhoto from '../images/gallery.png'
 import TeamPhoto from '../images/DemoDay.png'
 import Database from '../images/Database.png'
 import Database1 from '../images/TeamPhotos/inclasssketch.jpg'
+import Database2 from '../images/TeamPhotos/DatabaseEntityRelationDiagram.png'
+import Database3 from '../images/TeamPhotos/DatabaseTableDiagram.png'
+import Database4 from '../images/TeamPhotos/DatabaseUMLDiagram.png'
+import App1 from '../images/TeamPhotos/app1.png'
+import App2 from '../images/TeamPhotos/Section_Sketch.jpg'
+import APIImage from '../images/TeamPhotos/API.png'
+import Tracking1 from '../images/TeamPhotos/Tracking(1).png'
 // import Regensburg from '../images/city.jpg'
 import COSIT from '../images/cosit.png'
 import SCRUM from '../images/scrum.png'
@@ -95,14 +102,16 @@ function PHistory() {
                             <li>Administrative access to database </li>
                             </ul>
 
-                            <h5> Mobile App Team </h5>
+                            <h4> Mobile App Team </h4>
                             <ul>                     
                             <li>ADA compliance</li> 
                             <li>Implemented audible aspects of app</li>
                             <li>Created pages with leveled descriptions of art</li>
                             </ul>
+                            <img src = {App1} className = 'picture1' alt='App Idea Sketch'></img>
+                            <img src = {App2} className = 'picture1' alt='App Layout'></img>
 
-                            <h5> UX/UI Team</h5>
+                            <h4> UX/UI Team</h4>
                             <ul>                     
                             <li>Completed Coyote Verbal Description Training</li> 
                             <li>Completed basic, spatial, and thematic painting descriptions for 5 paintings, which completed a corner of the gallery.</li>
@@ -112,31 +121,46 @@ function PHistory() {
                             </ul>
                             
 
-                            <h6>Alexa Team</h6>
+                            <h4>Alexa Team</h4>
                             <ul>
                             <li>Created team server</li>
                             <li>Connected to the database using https requests through server</li>
                             <li>Updated past model with new descriptions and information</li>
                             </ul>
 
-                            <h7>Database Team</h7>
+                            <h4>Database Team</h4>
                             <ul>
                             <li>Fully functioning relational SQL database used to represent the Bowdoin Art Gallery hosted at ClearDB</li>
                             <li>Node.js API Server, hosted at Heroku. API endpoints are documented using Swagger</li>
                             <li>API Server developed using a special npm package, “nodespull,” built by our very own, Isaac Kabuika</li>
+                            <img src = {APIImage} className = 'picture1' alt='API'></img>
                             <li>Map model using artwork adjacency and zoning. The map is modeled as a graph, where nodes are artworks.</li>
                             <li>Json Web Token authentication (paired with admin level accounts) allows for art museum managers to administer the database – via the portal built by the Website Team.</li>
                             </ul>
                             <p>Here is the development of the Database: </p>
                             <img src = {Database1} className = 'picture1' alt='Database Concept Sketch'></img>
+                            <img src = {Database2} className = 'picture1' alt='Database Entity Relations'></img>
+                            <img src = {Database3} className = 'picture1' alt='Database Table Diagram'></img>
+                            <img src = {Database4} className = 'picture2' alt='Database UML Diagram'></img>
                             
 
-                            <h8>Data Collection</h8>
+                            <h4>Data Collection</h4>
                             <ul>
                             <li>Served as a liaison between other groups and the museum by responding to specific teams needs and data requests.</li>
                             <li>Began the development of a virtual tour using the Google Virtual Your creator.</li>
                             <li>Incorporated accessibility features into the google tour such as audio descriptions and picture enlargement. </li>
                             </ul>
+
+                            <h4>Tracking Team</h4>
+                            <ul>
+                            <li>Extracted positioning data from Arduino and iPhone IMUs </li> 
+                            <li>Implemented Common Filter for sensor data</li>   
+                            <li>Determined x,y position of an iPhone using Estimotes</li>
+                            <li>Integrated with mobile app</li>
+                            </ul>
+                            <img src = {Tracking1} className = 'picture1' alt='Tracking Schema'></img>
+
+
 
 
                         </TimelineItem>
